20efffe6411e6ae43ad9c9ed8563cf024b104fca
ArtemPeshkov <a.o.peshkov@yandex.ru>
2023-07-30 00:40:30 +0500

    test commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// const a =100;
+// a = 20;
+// console.log(a);
+
+// console.log(a)
+// let a = 15;
+// console.log(a)
+
+// console.log(a);
+// var a;
+// a = 10;
+// console.log(a);
+
+let testA;
+
+testA = "a";
+
+let testB ="b";
+
+console.log(typeof testA);
+console.log(typeof testB);
+
+testA = Number(testA);
+
+console.log(testA);
+console.log(typeof(testA));
+
+
